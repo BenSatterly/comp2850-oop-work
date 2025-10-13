f1703d64e47857cb065fc0587ae19b6ab29f326d
Ben Satterly <dwbt7640@leeds.ac.uk>
2025-10-13 17:50:07 +0000

    Message

--- portfolio/wordle/src/test/kotlin/WordleTest.kt
+++ portfolio/wordle/src/test/kotlin/WordleTest.kt
@@ -5,20 +5,20 @@ import io.kotest.matchers.shouldBe
 @Suppress("unused")
 class WordleTest : StringSpec({
 
-    "Tests for isValid function" {
+    "isValid - Tests that function correctly validates data entry" {
         isValid("TESTS") shouldBe true
         isValid("NOPE") shouldBe false
         isValid("YESSIR") shouldBe false
         isValid("") shouldBe false
     }
 
-    "Randomly picked word should be 5 letters long, and from the list" {
+    "pickRandomWord - Randomly picked word should be 5 letters long, and from the list" {
         val words = readWordList("words.txt")
         pickRandomWord(words).length shouldBe 5
         words.contains(pickRandomWord(words)) shouldBe true
     }
 
-    "evaluateGuess function should return the correct comparison with target word" {
+    "evaluateGuess - Function should return the correct comparison with target word" {
         val words = readWordList("words.txt")
         val target = pickRandomWord(words)
 
@@ -26,9 +26,8 @@ class WordleTest : StringSpec({
         evaluateGuess(target, target) shouldBe mutableListOf(1, 1, 1, 1, 1)
     }
 
-    "word list (from input words file) should be a list with multiple elements (words)" {
+    "readWordList - Word list (from input words file) should have multiple elements (words)" {
         val words = readWordList("words.txt")
-        words::class.simpleName shouldBe "MutableList"
         (words.count() > 1) shouldBe true
     }
 })
