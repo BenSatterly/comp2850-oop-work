f7da7cbcc968a8b0dd69e243ca6006f8754ae4e8
Ben Satterly <dwbt7640@leeds.ac.uk>
2025-10-13 09:37:50 +0000

    Tests

--- portfolio/wordle/src/test/kotlin/WordleTest.kt
+++ portfolio/wordle/src/test/kotlin/WordleTest.kt
@@ -4,5 +4,30 @@ import io.kotest.matchers.shouldBe
 
 @Suppress("unused")
 class WordleTest : StringSpec({
-    // Write your tests here
+    // Code for function 'isValid' testing goes here {
+        isValid("TESTS") shouldBe true
+        isValid("NOPE") shouldBe false
+        isValid("YESSIR") shouldBe false
+        isValid("") shouldBe false
+    }
+    
+    // Code for function 'pickRandomWord' testing goes here {
+        var words = readWordList("words.txt")
+        (pickRandomWord(words)).length() shouldBe 5
+        words.contains(pickRandomWord(words)) shouldBe true
+    }
+
+    // Code for function 'evaluateGuess' testing goes here {
+        var words = readWordList("words.txt")
+        val target = pickRandomWord(words)
+
+        evaluateGuess("XXXXX", target) shouldBe [0, 0, 0, 0, 0]
+        evaluateGuess(target, target) shouldBe [1, 1, 1, 1, 1]
+    }
+
+    // Code for function 'readWordList' testing goes here {
+        var words = readWordList("words.txt")
+        words::class.simpleName shouldBe "List"
+        words.count()>1 shouldBe true
+    }
 })
